5d67ed2c98d4247e9e2caae8620f45cf3ab6db3e
王巍 <wei.wang90@foxmail.com>
2016-11-23 21:39:07 +0800

    finish website

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'topics#index'
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+    member do
+      post "downvote"
+    end
+  end
 end
